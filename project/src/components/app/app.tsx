101d9aa0040699b0a1c3c363a71b8b6a8e8ca0d7
Maria Echkenko <mashasly@mail.ru>
2022-02-13 00:13:46 +0300

    Создает компонент главной страницы

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,25 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppProps = {
+  promoFilmName: string;
+  promoFilmGenre: string;
+  promoFilmYear: number;
+  films: {
+    id: number;
+    name: string;
+    previewImage: string;
+  }[];
+}
+
+function App({promoFilmName, promoFilmGenre, promoFilmYear, films}: AppProps): JSX.Element {
+  return (
+    <MainPage
+      promoFilmName= {promoFilmName}
+      promoFilmGenre= {promoFilmGenre}
+      promoFilmYear= {promoFilmYear}
+      films = {films}
+    />
+  );
 }
 
 export default App;
