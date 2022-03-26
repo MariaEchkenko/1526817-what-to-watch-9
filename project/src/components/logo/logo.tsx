1bef23c16d2fc355a25d28a0a991795193d2b01c
Maria Echkenko <mashasly@mail.ru>
2022-03-26 10:12:46 +0300

    Добавляет сброс счетчика и жанра при возврате на главную

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
+import { useAppDispatch } from '../../hooks/';
+import { changeGenre, resetFilmCount } from '../../store/action';
 import { LogoMod } from '../../types/logo';
-import { AppRoute } from '../../const';
+import { ALL_GENRES, AppRoute } from '../../const';
 
 type LogoProps = LogoMod;
 
 function Logo({classMod='logo__link'}: LogoProps): JSX.Element {
+  const dispatch = useAppDispatch();
   return (
     <div className="logo">
-      <Link className={classMod} to={AppRoute.Main}>
+      <Link
+        className={classMod}
+        to={AppRoute.Main}
+        onClick={() => {
+          dispatch(resetFilmCount());
+          dispatch(changeGenre(ALL_GENRES));
+        }}
+      >
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
