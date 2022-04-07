4999678519483204d1575c6b9dd72641996f244a
Maria Echkenko <mashasly@mail.ru>
2022-04-07 20:58:29 +0300

    Реализует время проигрывания плеера

--- project/src/pages/player/player.tsx
+++ project/src/pages/player/player.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
+import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 import { useAppSelector } from '../../hooks/';
-import { useParams, useNavigate, Navigate } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { useParams, useNavigate } from 'react-router-dom';
+import Loader from '../../components/loader/loader';
+import Error from '../../components/error/error';
 import { selectFilms } from '../../store/films-data/selectors';
 
+dayjs.extend(duration);
 
 function Player(): JSX.Element {
   const films = useAppSelector(selectFilms);
@@ -11,6 +15,10 @@ function Player(): JSX.Element {
   const film = films.find((movie) => movie.id === Number(id));
 
   const [isActive, setIsActive] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const navigate = useNavigate();
 
@@ -32,17 +40,45 @@ function Player(): JSX.Element {
   };
 
   const handleFullScreen = () => {
-    videoRef.current?.requestFullscreen();
+    if (videoRef.current) {
+      videoRef.current.requestFullscreen();
+    }
   };
 
   const handleExit = () => {
-    videoRef.current?.pause();
-    navigate(-1);
+    if (videoRef.current) {
+      videoRef.current.pause();
+      navigate(-1);
+    }
+  };
+
+  const handleTimeUpdate = () => {
+    if (videoRef.current) {
+      setCurrentTime(videoRef.current.currentTime);
+      setProgress((videoRef.current.currentTime / videoRef.current.duration) * 100);
+    }
+  };
+
+  const formatRunTime = () => {
+    if (videoRef.current) {
+      const runTime = videoRef.current.duration - currentTime;
+      return dayjs.duration(runTime, 'seconds').format('HH:mm:ss');
+    }
+  };
+
+  const handleLoaded = () => {
+    if (videoRef.current) {
+      setIsLoading(false);
+    }
   };
 
   if (!film) {
-    return <Navigate to={AppRoute.Main} />;
+    return <Error />;
   }
+
+  /*if (loading) {
+    return <Loader />;
+  }*/
 
   return (
     <div className="player">
@@ -51,57 +87,56 @@ function Player(): JSX.Element {
         src={film.videoLink}
         className="player__video"
         poster={film.posterImage}
+        onTimeUpdate={handleTimeUpdate}
+        onLoadedMetadata={handleLoaded}
       >
       </video>
 
       <button
         type="button"
         className="player__exit"
-        onClick={() => {
-          handleExit();
-        }}
+        onClick={handleExit}
       >
         Exit
       </button>
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: '30%'}}>Toggler</div>
+      {isLoading
+        ? <Loader />
+        :
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress className="player__progress" value={progress} max="100"></progress>
+              <div className="player__toggler" style={{left: `${progress}%`}}>Toggler</div>
+            </div>
+            <div className="player__time-value">{formatRunTime()}</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
 
-        <div className="player__controls-row">
-          <button
-            type="button"
-            className="player__play"
-            onClick={() => {
-              handleFilmActive();
-            }}
-          >
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              {isActive ? <use xlinkHref="#pause"></use> : <use xlinkHref="#play-s"></use>}
-            </svg>
-            <span>{isActive ? 'Pause' : 'Play'}</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__controls-row">
+            <button
+              type="button"
+              className="player__play"
+              onClick={handleFilmActive}
+            >
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                {isActive ? <use xlinkHref="#pause"></use> : <use xlinkHref="#play-s"></use>}
+              </svg>
+              <span>{isActive ? 'Pause' : 'Play'}</span>
+            </button>
+            <div className="player__name">Transpotting</div>
 
-          <button
-            type="button"
-            className="player__full-screen"
-            onClick={() => {
-              handleFullScreen();
-            }}
-          >
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
-        </div>
-      </div>
+            <button
+              type="button"
+              className="player__full-screen"
+              onClick={handleFullScreen}
+            >
+              <svg viewBox="0 0 27 27" width="27" height="27">
+                <use xlinkHref="#full-screen"></use>
+              </svg>
+              <span>Full screen</span>
+            </button>
+          </div>
+        </div>}
     </div>
   );
 }
